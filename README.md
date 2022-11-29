256ab4e14433123b37419da968554052143d5112
VALdeilSONn <86812071+VALdeilSONn@users.noreply.github.com>
2022-11-29 20:19:15 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# PrimeiroTabalhoReact
-Trabalho de Squad para a Resilia
+## Welcome to my React CRUD Operations tutorials Video
+
+Here's the link : https://www.youtube.com/watch?v=-ZMP8ZladIQ&ab_channel=Cybernatico
+
+If you enjoyed this piece, please do show some love with a like, comment, or just click on subscribe button. Every bit matters :)
+
+### My Portfolio: https://nishant666.online/
+### Twitter: https://twitter.com/nishants440
+### freeCodeCamp: https://www.freecodecamp.org/news/author/nishant-kumar/
+### Github: https://github.com/nishant-666
---
+++ src/components/update/update.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import { useHistory, Link } from 'react-router-dom';
+export default function Update() {
+    let history = useHistory();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [ID, setID] = useState(null);
+    const sendDataToAPI = () => {
+        axios.put(`https://js0n-server.vercel.app/GPU/${ID}`, {
+            firstName,
+            lastName
+        }).then(() => {
+            history.push('/read')
+        })
+    }
+
+    useEffect(() => {
+        setFirstName(localStorage.getItem('firstName'));
+        setLastName(localStorage.getItem('lastName'));
+        setID(localStorage.getItem('ID'))
+    }, [])
+
+    return (
+        <div>
+            <Form>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input name="fname"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        placeholder='First Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        name="lname"
+                        value={lastName}
+                        placeholder='Last Name'
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Form.Field>
+                <Button type='submit' onClick={sendDataToAPI}>Update</Button>
+            </Form>
+
+            <div>
+            <Link to="/read">Início</Link>
+            </div>
+
+        </div>
+    )
+}
